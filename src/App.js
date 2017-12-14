bdd51d86e1009267c1bf84ec91a2d530a5be7bfd
Paul <pfreema1@gmail.com>
2017-12-13 20:13:32 -0500

    learned child to parent comm

--- src/App.js
+++ src/App.js
@@ -5,14 +5,50 @@ class App extends Component {
   render() {
     return (
       <div className="main-container">
+        <MainTimerSection />
+      </div>
+    );
+  }
+}
+
+class MainTimerSection extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerMode: 25,
+      isRunning: false
+    };
+
+    this.handlePlayPauseClick = this.handlePlayPauseClick.bind(this);
+  }
+
+  handlePlayPauseClick(e) {
+    e.preventDefault();
+
+    console.log("PARENT IS NOTIFIED!");
+
+    this.setState({
+      isRunning: this.state.isRunning ? false : true
+    });
+
+  }
+
+  render() {
+    return(
+      <div onClick={this.handleClick}>
+        <div className="time-and-play-reset-button-container">
+          <TimeDisplay 
+            isRunning={this.state.isRunning}
+            
+          />
+          <PlayPauseResetButtons 
+            isRunning={this.state.isRunning}
+            playPauseHandler={this.handlePlayPauseClick}
+          />
+        </div>
         <div>
-          <div className="time-and-play-reset-button-container">
-            <TimeDisplay />
-            <PlayPauseResetButtons />
-          </div>
-          <div className="time-mode-select-container">
-            <TimeModeSelect />
-          </div>
+          <TimeModeSelect />
         </div>
       </div>
     );
@@ -22,10 +58,58 @@ class App extends Component {
 
 class TimeDisplay extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: 1500, //1500 seconds = 25:00
+      formattedTimeString: "25:00"
+    };
+  }
+
+  componentDidMount() {
+    if(this.props.isRunning) {
+      this.intervalId = setInterval(() => this.updateTime(), 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  setFormattedTime(currentSeconds) {
+    
+    let minutes = Math.floor(currentSeconds/60);
+    let seconds = currentSeconds - minutes * 60;
+
+    let minutesString = minutes.toString();
+    //if seconds is less than 10 then add a 0 before the single digit
+    let secondsString = seconds < 10 ? "0" + seconds.toString() : seconds.toString();
+
+    
+
+    this.setState({
+      formattedTimeString: minutesString + ":" + secondsString
+    });
+  }
+
+  updateTime() {
+    
+    let currentSeconds = this.state.seconds - 1;
+
+    this.setFormattedTime(currentSeconds);
+
+    this.setState({
+      seconds: currentSeconds
+    });
+  }
+
+  
+
   render() {
     return(
       <div className="time-display">
-        23:05
+        {this.state.formattedTimeString}
       </div>
 
     );
@@ -35,10 +119,15 @@ class TimeDisplay extends Component {
 
 class PlayPauseResetButtons extends Component {
 
+  
+
   render() {
     return (
       <div className="play-pause-reset-buttons-container box-shadow">
-        <PlayAndPauseButton />
+        <PlayAndPauseButton 
+          isRunning={this.props.isRunning}
+          playPauseHandler={this.props.playPauseHandler}
+        />
         <ResetButton />
       </div>
     );
@@ -47,10 +136,17 @@ class PlayPauseResetButtons extends Component {
 
 class PlayAndPauseButton extends Component {
 
+
+
+
   render() {
     return (
-      <div className="play-pause-button-container">
-        <i className="icon-play"></i>
+      <div 
+        className="play-pause-button-container" 
+        onClick={this.props.playPauseHandler}
+      >
+        
+        <i className={this.props.isRunning ? "icon-pause" : "icon-play"}></i>
       </div>
     );
   }
@@ -74,12 +170,48 @@ class TimeModeSelect extends Component {
 
   render() {
     return (
-      <div className="box-shadow">
-        foo
+      <div className="time-mode-select-container box-shadow">
+        <PomodoroButton />
+        <ShortBreakButton />
+        <LongBreakButton />
       </div>
     );
   }
 }
+
+
+class PomodoroButton extends Component {
+
+  render() {
+    return (
+      <div className="time-mode-button">
+        Pomodoro
+      </div>
+    );
+  }
+}
+
+class ShortBreakButton extends Component {
+
+  render() {
+    return (
+      <div className="time-mode-button">
+        Short<br/>Break
+      </div>
+    );
+  }
+}
+
+  class LongBreakButton extends Component {
+    
+    render() {
+      return (
+        <div className="time-mode-button">
+          Long<br/>Break
+        </div>
+      );
+    }
+  }
 
 
 
