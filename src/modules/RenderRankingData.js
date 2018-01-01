81853187908fd3c1d76bc50e05f8c1f271882f9b
Paul <pfreema1@gmail.com>
2018-01-01 13:25:46 -0500

    meow

--- src/modules/RenderRankingData.js
+++ src/modules/RenderRankingData.js
@@ -206,58 +206,7 @@ export default function renderRankingData(width, height, data) {
 
 
 
-//     var node = canvas.selectAll(".node")
-//         .data(nodes)
-//         .enter()
-//         .append("g")
-//         .attr("class", "node")
-//         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; } );
-        
-    
-    
-//     //append a circle to each node - before this point, only the elements were created in the dom and given a class above
-//     node.append("circle")
-//         .attr("id", function(d) { 
-//         if(d.name === "parent") {
-//             return "";
-//         } else {
-//             return d.userId; 
-//         }
-//         })
-//         .attr("r", function(d) { return d.r; })
-//         .attr("fill", function(d) { return d.username ? colorScale(d.pomodoros) : "yellow"})
-//         .attr("opacity", function(d) { return d.username ? 0.75 : 0.1})
-//         .attr("stroke", "black")
-//         .attr("stroke-width", 3)
-//         .attr("stroke-opacity", 1.3);
-    
-//     //setup clip path so text doesnt overflow it's circle
-//     node.append("clipPath")
-//         .attr("id", function(d) { 
-//         if(d.name === "parent") {
-//             return "";
-//         } else {
-//             return "clip-" + d.userId; 
-//         }
-//         })
-//         .append("use")
-//         .attr("xlink:href", function(d) { return "#" + d.userId; });
-    
 
-//     node.append("text")
-//         .attr("clip-path", function(d) { 
-//         return "url(#clip-" + d.userId + ")"; 
-//         })
-//         .text(function(d) {
-//         if(d.username) {
-//             // console.log(d.characterNum);
-//             return funnyFacesArray[d.characterNum];
-//         }
-        
-//         })
-//         .attr("text-anchor", "middle")
-//         .attr("font-family", "sans-serif")
-//         .attr("font-size", "30");
 }
 
 
